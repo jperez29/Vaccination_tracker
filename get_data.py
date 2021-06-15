e6c45f02a7324b55c7b3f2ac803b3d2e564dbb47
Clariza Mayo <clarizamayo@MacBook-Pro.fios-router.home>
2021-06-14 22:38:49 -0400

    update to get_data.py

--- get_data.py
+++ get_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import datetime
 import kaggle
 from sqlalchemy import create_engine
@@ -7,27 +6,17 @@ from sqlalchemy import create_engine
 engine = create_engine('sqlite:///dash/project_2.db', echo=False)
 
 links = ["berkayalan/vaccinations-in-united-states"]
-def get_vac_data(): # Split this into get_stroke_data and parse_stroke_data
+def get_vac_data(): 
     api = kaggle.KaggleApi()
     api.authenticate()
     for link in links:
         api.dataset_download_files(link, unzip=True, quiet=False)
-    vacc = pd.read_csv("us_vaccinations.csv", sep=';')
-    
-    start = datetime.datetime.now()
-    while True:
-        if datetime.datetime.now() > (start +  datetime.timedelta(seconds=1)):
-            api.authenticate()
-            for link in links:
-                api.dataset_download_files(link, unzip=True, quiet=False)
-            vacc = pd.read_csv("us_vaccinations.csv", sep=';')
-
-        yield vacc
+    vacc = pd.read_csv("us_state_vaccinations.csv", sep=';')
+    return vacc
 
 def creating_db():  
-    dataframes = get_vac_data()
-    vacc = next(dataframes)
-    moving_vac_to_db = vacc.to_sql('vacc_2', con=engine, if_exists='append')
+    vacc = get_vac_data()
+    moving_vac_to_db = vacc.to_sql('vacc', con=engine, if_exists='append')
 
 def check_db(table_name):
     df= pd.read_sql_table(table_name, con=engine)
